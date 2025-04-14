71ec696cf8ff9704cf87df3bee2d798fc4b150fc
hipopodelfuego <129762797+hipopodelfuego@users.noreply.github.com>
2025-04-14 12:07:33 +0200

    mini reglage

--- app/src/main/java/com/example/projet_mob/SoloGame.kt
+++ app/src/main/java/com/example/projet_mob/SoloGame.kt
@@ -47,6 +47,7 @@ class SoloGame : Activity() {
         findViewById<Button>(R.id.btnRandom).setOnClickListener {
             randomSequence = activities.shuffled().take(3)
             currentIndex = 0
+            totalScore = 0
             launchNextActivity()
         }
     }
